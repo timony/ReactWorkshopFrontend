6b73162962d8d2c5e6098017b32e7e2d7939ebfd
Mika Tomas <tomas.mika@tieto.com>
2018-01-19 14:26:13 +0100

    Inject js into html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,12 @@
 import babelify from 'babelify';
 import browserify from 'browserify';
+import config from './build.config';
 import connect from 'gulp-connect';
 import del from 'del';
+import inject from 'gulp-inject';
 import gulp from 'gulp';
 import open from 'gulp-open';
+import order from 'gulp-order';
 import source from 'vinyl-source-stream';
 
 const gulpsync = require('gulp-sync')(gulp);
@@ -14,26 +17,42 @@ gulp.task('clean', () => {
 
 gulp.task('js', () => {
   const options = {
-    entries: './src/App.jsx',   // Entry point
+    entries: 'src/App.jsx',   // Entry point
     extensions: ['.js', '.jsx'],            // consider files with these extensions as modules
     debug: true,  // add resource map at the end of the file or not
-    paths: ['./src/']           // This allows relative imports in require, with './scripts/' as root
+    paths: ['src/']           // This allows relative imports in require, with './scripts/' as root
   };
 
   return browserify(options)
     .transform(babelify)
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('html', () => {
-  gulp
-    .src('src/index.html')
+  const target = gulp.src('src/*.html');
+  const sources = gulp.src([
+      'dist/css' + '/*.css',
+      'dist/js' + '/*.js'
+    ],
+    {read: false}
+  );
+
+  const ordering = config.vendor.js.map(filePattern => path.basename(filePattern));
+  ordering.unshift('*.css');
+
+  const orderedSources = sources.pipe(order(ordering));
+
+  return target
+    .pipe(inject(orderedSources, {
+      ignorePath: 'dist',
+      addRootSlash: false
+    }))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('transpile', gulpsync.sync(['clean', ['js', 'html']]));
+gulp.task('transpile', gulpsync.sync(['clean', 'js', 'html']));
 
 gulp.task('start-server', ['transpile'], () => {
   const connectOptions = {
